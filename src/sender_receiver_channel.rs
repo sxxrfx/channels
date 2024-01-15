aaa3549ef42aad7f8a78f7a29a8cb2096c372c29
Sagar Negi <sxxrfx@gmail.com>
2024-01-15 12:59:34 +0530

    fix: added safety and test; drop interface for channel<T>

--- src/sender_receiver_channel.rs
+++ src/sender_receiver_channel.rs
@@ -32,6 +32,7 @@ unsafe impl<T> Sync for Channel<T> where T: Send {}
 
 impl<T> Sender<T> {
     pub fn send(self, message: T) {
+        // Safety: Only one writer and no reader
         unsafe { (*self.channel.message.get()).write(message) };
         self.channel.ready.store(true, Ordering::Release);
     }
@@ -40,5 +41,47 @@ impl<T> Sender<T> {
 impl<T> Receiver<T> {
     pub fn is_ready(&self) -> bool {
         self.channel.ready.load(Ordering::Relaxed)
+    }
+
+    pub fn receive(&self) -> T {
+        if !self.channel.ready.swap(false, Ordering::Acquire) {
+            panic!("no message available!");
+        }
+
+        // Safety: No writer and only reader
+        unsafe { (*self.channel.message.get()).assume_init_read() }
+    }
+}
+
+impl<T> Drop for Channel<T> {
+    fn drop(&mut self) {
+        if *self.ready.get_mut() {
+            // Safety: `self.ready` == `true` so, `self.message` is intialized
+            unsafe { (*self.message.get_mut()).assume_init_drop() }
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use std::thread;
+
+    use super::*;
+    #[test]
+    fn sender_receiver() {
+        let (tx, rx) = channel();
+
+        let t = thread::current();
+        thread::scope(|s| {
+            s.spawn(|| {
+                tx.send("hello, world");
+                t.unpark();
+            });
+            if !rx.is_ready() {
+                thread::park();
+            }
+
+            assert_eq!(rx.receive(), "hello, world");
+        })
     }
 }
